d9b930c576cd8d2977d5a18baee23c9233b5552d
Grzegorz Beringer <grzegorz.beringer@gmail.com>
2019-01-12 15:15:05 +0000

    Add top-1 and top-5 accuracy to testing script

--- test.py
+++ test.py
@@ -30,7 +30,11 @@ def main():
     c = config.context
 
     cos_dists = defaultdict(list)
+    top1_scores = defaultdict(list)
+    top5_scores = defaultdict(list)
     cos_dists_all = []
+    top1_scores_all = []
+    top5_scores_all = []
     for _, sample in test_texts_df.iterrows():
         path = sample['path']
         keyword = sample['keyword']
@@ -45,13 +49,28 @@ def main():
         cos_dist = utils.cos_dist(context_embedding, keyword_embeddings[keyword])
         cos_dists[keyword].append(cos_dist)
         cos_dists_all.append(cos_dist)
+        
+        top5_kws = [kw for kw, _ in utils.get_top_k_closest_keywords(context_embedding, keyword_embeddings)]
+        is_top1 = int(keyword == top5_kws[0])
+        is_top5 = int(keyword in top5_kws)
+        top1_scores[keyword].append(is_top1)
+        top5_scores[keyword].append(is_top5)
+        top1_scores_all.append(is_top1)
+        top5_scores_all.append(is_top5)
+        # print('{}, {}, {}, {}, {}'.format(keyword, top5_kws, is_top1, is_top5, cos_dist))
     
-    print('\n<keyword>: <avg cosine distance(keyword, context)>')
-    print('----------------------------------------------------')
+    print('\n---------------------------------------------------------------')
+    print('| {} | avg cos dist | top-1 acc | top-5 acc |'.format(
+        utils.bold('keyword'.ljust(20))))
+    print('|-------------------------------------------------------------|')
     for kw, dists in cos_dists.items():
-        print('{}: {:.6f}'.format(utils.bold(kw), np.mean(dists)))
-    print('----------------------------------------------------')
-    print('Average cosine distance: {:.6f}\n'.format(np.mean(cos_dists_all)))
+        print('| {} |   {:.6f}   |   {:.2f}    |   {:.2f}    |'.format(
+            utils.bold(kw.ljust(20)), np.mean(dists), np.mean(top1_scores[kw]), np.mean(top5_scores[kw]))
+        )
+    print('---------------------------------------------------------------')
+    print('\nAvg cosine distance: {:.6f}'.format(np.mean(cos_dists_all)))
+    print('Top-1 acc:           {:.2f}'.format(np.mean(top1_scores_all)))
+    print('Top-5 acc:           {:.2f}\n'.format(np.mean(top5_scores_all)))
 
 
 if __name__ == "__main__":
